fa736cd676eb2a87a7296cc54819fa2c24ff4777
KARIMELAISSAOUY <karimelaissaouy1@gmail.com>
2023-11-27 18:32:48 -0800

    0x00-ES6_basic

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,11 @@
 export default function getNeighborhoodsList() {
+  /*eslint_disable*/
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
 }
+
