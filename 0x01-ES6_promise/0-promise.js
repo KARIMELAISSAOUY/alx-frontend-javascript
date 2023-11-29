68301ac69c6ad443610f4231d3d1466454bef2ff
ABDELKARIM ELAISSAOUY <118484209+KARIMELAISSAOUY@users.noreply.github.com>
2023-11-29 23:57:18 +0000

    0x01-ES6_promise
    0x01-ES6_promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getRespondFromAPI(){
+		/*Eslint-disable*/
+  return new Promise((resolve, reject) => {
+       if (true) {
+	resolve();
+      } else {
+	reject();
+      }
+  });
+}
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,7 @@
+# 0x01. ES6 Promises
+```JavaScript``` | ```ES6```
+---
+### ES6 Promises
+---
+>0x01
+
