862a2338f34819269c8f44edf0afdc82a4dc4901
KARIMELAISSAOUY <karimelaissaouy1@gmail.com>
2023-12-01 15:51:30 -0800

    0x01-ES6_promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  //eslint-disable
-  return Promise
-    .resolve({ firstName, lastName });
+  return Promise.resolve({
+    firstName: `${firstName}`,
+    lastName: `${lastName}`,
+  });
 }
