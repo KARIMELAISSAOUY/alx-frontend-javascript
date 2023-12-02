9c0a37ee22ae1aca4513fe8b47b6a097dd0d0a22
ABDELKARIM ELAISSAOUY <118484209+KARIMELAISSAOUY@users.noreply.github.com>
2023-12-02 01:24:05 +0000

    0x00-ES6_basic
    0x00-ES6_basic

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,9 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;//false
+  const task2 = true;//true
+
+  if (trueOrFalse) {
+    return [task, task2];
+  }
+  return [task, task2];
+}
